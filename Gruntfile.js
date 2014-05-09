948df52f056e757816e9494264187f2a76977445
Tom Blanchard <yo@tomblanchard.co.uk>
2014-05-09 19:05:48 +0100

    Changed loads

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,7 +27,7 @@ module.exports = function(grunt) {
     sass: {
       dist: {
         options: {
-          style: 'expanded'
+          style: 'compressed'
         },
         files: [{
           expand: true,
